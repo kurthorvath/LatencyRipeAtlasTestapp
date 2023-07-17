71a489afe8b6482c65762100118aa809a356393c
Kurt Horvath <kurtklaus.horvath@gmail.com>
2023-07-17 22:33:26 +0200

    Update mainApp.py
    add near shore nodes

--- mainApp.py
+++ mainApp.py
@@ -15,10 +15,14 @@ def procTask(src, dst):
     return createFullMeasure(src, dst)
 
 storage = IOwrapper("inp.xlsx")
-storage.addNode(Node("Arnes","si-lju-as2107.anchors.atlas.ripe.net", 6861, "193.2.63.12","2001:1470:8000:603::12"))
-storage.addNode(Node("Anexia","at-klu-as42473.anchors.atlas.ripe.net", 6120, "178.255.156.202","2a00:11c0::1:0:0:3e"))
-storage.addNode(Node("BIX","hu-bud-as12303.anchors.atlas.ripe.net", 6039, "5.28.0.17","2a00:e6a0:3:1011"))
-storage.printAllNodes()
+#storage.addNode(Node("Arnes","si-lju-as2107.anchors.atlas.ripe.net", 6861, "193.2.63.12","2001:1470:8000:603::12"))
+#storage.addNode(Node("Anexia","at-klu-as42473.anchors.atlas.ripe.net", 6120, "178.255.156.202","2a00:11c0::1:0:0:3e"))
+#storage.addNode(Node("BIX","hu-bud-as12303.anchors.atlas.ripe.net", 6039, "5.28.0.17","2a00:e6a0:3:1011"))
+
+storage.addNode(Node("Feldkirchen-oja","", 52690, "77.220.115.19",""))
+storage.addNode(Node("Villach-net4you","", 15439, "5.28.0.17","194.177.153.171")) #AS6798 
+storage.addNode(Node("Hermagor-NETcompany ","", 50074, "185.56.124.18","")) #AS50226
+
 
 for src in storage.nodes:
     for dst in storage.nodes:
